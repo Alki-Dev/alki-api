2aea63e93ae4fb56e1be1eb78681a6782d2c7abc
Marc Marina <marc.marina.miravitlles@gmail.com>
2023-01-12 13:38:17 +0000

    Simplified logger

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -1,15 +1,3 @@
 import { createLogger } from './create-logger';
 
-const pinoLogger = createLogger();
-
-export const logger = {
-  error: (error: Error) => {
-    pinoLogger.error(error);
-  },
-  debug: (msg: string, args?: Record<string, unknown>) => {
-    pinoLogger.debug({ ...args }, msg);
-  },
-  info: (msg: string, args?: Record<string, unknown>) => {
-    pinoLogger.info({ ...args }, msg);
-  },
-};
+export const logger = createLogger();
